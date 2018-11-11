9a9604706884eb5eadf204dcb1977bcfb77b697d
mixassio <mixail.bondarev@gmail.com>
2018-11-11 18:45:02 +0300

    first part

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import products from './constants/products';
+import ProductCard from './components/productCard';
 
-class App extends Component {
+class Catalog extends Component {
   constructor(props) {
     super(props);
+    this.state = { productsList: [] };
   }
-  
+  componentDidMount() {
+    this.setState({ productsList: products() });
+  }
   render() {
     return (
-      <div>Hello, world</div>
+      <div>
+        <ul>
+          {
+            this.state.productsList.map((prod) => (
+              <li key={prod.id}><ProductCard prod={prod} /></li>
+            ))
+          }
+        </ul>
+      </div>
     );
   }
 }
 
 ReactDom.render(
-  <App />,
+  <Catalog />,
   document.getElementById('root')
 )
